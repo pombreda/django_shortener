51735732d9a7f420748dc4bf588c6f0571688cc4
Olivier Demah <olivier@foxmask.info>
2012-09-24 23:35:37 +0200

    improvement of the shortener - switch the md5 on the urlfull instead of the urlshort

--- views.py
+++ views.py
@@ -31,29 +31,60 @@ def home(request):
     return render_to_response('django_shortener/home.html', context)
 """
     save a new full url in a short way
+    1) we check the POST request
+    2) we validate the form
+    3) we calculate the MD5 of the urlfull field
+    4) we check if that md5 is already in the database
+    5) if yes ; we stop
+    6) if no ; we store the datas
 """
 def new_shrt(request):
     if request.method == 'POST':
         form = ShrtForm(request.POST)
         if form.is_valid():
+            #get the Model object
+            shrt_url = Shrt()
+            #create a md5sum on the urlfull
+            urlmd5 = hashlib.md5(form.cleaned_data['urlfull'])
+            shrt_url.urlmd5 = urlmd5.hexdigest()
+
             #get the size of the random shortener string
             length = settings.SHRT
             #let's generate the shortener string
             shrt = urlshortener()
             shortener = shrt.run(length["url_size"])
-            #create the "md5sum" of the shortener
-            urlmd5 = hashlib.md5(shortener)
 
-            if request.user.is_authenticated():
-                user = request.user
-            else:
-                user = User()
-                user.id = 0
-            new_shortener = form.save(commit=False)
-            new_shortener.urlshort = shortener
-            new_shortener.urlmd5 = urlmd5.hexdigest()
-            new_shortener.user = user
-            new_shortener.save()
+            from django.core.exceptions import ValidationError, NON_FIELD_ERRORS
+            try:
+                #check if the urlfull is unique by verifying its md5
+                shrt_url.validate_unique()
+                
+                #start an instance of the form
+                new_shortener = form.save(commit=False)                
+                
+                #1) urlshortener
+                #assign the shortener                 
+                new_shortener.urlshort = shortener
+                
+                #2) md5 of the urlfull
+                #assign the urlmd5 calculated "before" the "try/except"
+                new_shortener.urlmd5 = urlmd5.hexdigest()
+                
+                #3) 
+                #user ?
+                if request.user.is_authenticated():
+                    user = request.user
+                else:
+                    user = User()
+                    user.id = 0
+                #assign the user object
+                new_shortener.user = user
+                #finally : SAVE !
+                new_shortener.save()
+                
+            #validation error
+            except ValidationError as e:
+                non_field_errors = e.message_dict['urlmd5']
         
         #create a new instance for the new form :)
         form = ShrtForm()        
@@ -66,13 +97,10 @@ def new_shrt(request):
 """
 def show_shrt(request,shrt):       
     try:
-        #do a md5 on a shortener
-        urlmd5 = hashlib.md5(shrt)            
         #then let's search the url from its "MD5 sum"
-        urlfull = Shrt.objects.filter(urlmd5__exact=urlmd5.hexdigest())
+        urlfull = Shrt.objects.filter(urlshort__exact=shrt)
         context = {'shrt': urlfull, 'action':'show_shrt'}
         return render_to_response('django_shortener/home.html', context)
                 
     except Shrt.DoesNotExist:
         raise Http404
-    
