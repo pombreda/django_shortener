4e171908b373f2e3c52a613bb8867b1822d64a69
Olivier Demah <olivier@foxmask.info>
2012-09-29 17:22:21 +0200

    add setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+ 
+setup(
+    name='django_shortener',
+    version='0.1',
+    description='URL Shortener System',
+    author='Olivier Demah',
+    author_email='olivier@foxmask.info',
+    url='https://github.com/foxmask/django_shortener',
+    packages=['django_shortener'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities'        
+    ]
+)
